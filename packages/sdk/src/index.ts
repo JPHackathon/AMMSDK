77855517961dbe3735c447b871756be2ea87832f
Inaridiy <inainainariinainari@gmail.com>
2022-11-05 04:39:23 +0900

    feat: maybe created SDK

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import WeaveDB, { ArWallet, ArweaveConfig, EthWallet } from "weavedb-sdk";
 import DemoWallet from "./assets/wallet.json";
 
@@ -40,7 +41,7 @@ export class AMMSDK {
       wallet,
       contractTxId,
       name: "weavedb",
-      EthWallet: ethWallet,
+
       version: "1",
       arweave: arweave || {
         host: "arweave.net",
@@ -55,20 +56,88 @@ export class AMMSDK {
     return this.weaveDB.cget("games", address);
   }
 
-  async setGameData(data: GameData) {
-    await this.weaveDB.set(data, "games", data.signer);
-  }
-
   async relateUser(data: UserMapData) {
-    if (this.ethWallet.wallet !== data.game_address)
-      throw new Error("Invalid game address");
-    await this.weaveDB.add(data, "user_mappings");
+    await this.weaveDB.set(
+      data,
+      "user_mappings",
+      `${data.game_address}:${data.user_address}`,
+      this.ethWallet
+    );
   }
 
-  async getRate(pair: string) {
-    const pairData = await this.weaveDB.cget("pairs", pair);
-    return pairData.amount0 / pairData.amount1;
+  async getUserMapData(gameAddress: string, userAddress: string) {
+    const userMapData = await this.weaveDB.cget(
+      "user_mappings",
+      `${gameAddress}:${userAddress}`
+    );
+    return userMapData;
   }
 
-  async swap(game: string, amountIn: number) {}
+  async getPair(_game1: string, _game2: string) {
+    const [game1, game2] = [_game1, _game2].sort();
+    const pairData = await this.weaveDB.cget("pairs", `${game1}:${game2}`);
+    return pairData;
+  }
+
+  static getOutputAmount(
+    inputAmount: number,
+    inputReserve: number,
+    outputReserve: number
+  ) {
+    const inputAmountWithFee = inputAmount * 997;
+    const numerator = inputAmountWithFee * outputReserve;
+    const denominator = inputReserve * 1000 + inputAmountWithFee;
+    return numerator / denominator;
+  }
+
+  async swap(from: string, to: string, amountIn: number) {
+    if (!this.ethWallet.wallet) return;
+
+    const { data: pairData } = await this.getPair(from, to);
+    const { address0, address1, amount0, amount1 } = pairData;
+
+    if (Number(amount0) === 0 || Number(amount1) === 0)
+      throw new Error("Pair reserves are empty");
+
+    const amountOut = await AMMSDK.getOutputAmount(
+      amountIn,
+      from === address0 ? amount0 : amount1,
+      from === address1 ? amount1 : amount0
+    );
+
+    const [
+      { data: fromGameData },
+      { data: toGameData },
+      { data: fromUserData },
+      { data: toUserData },
+    ] = await Promise.all([
+      this.getGameData(from),
+      this.getGameData(to),
+      this.getUserMapData(from, this.ethWallet.wallet),
+      this.getUserMapData(to, this.ethWallet.wallet),
+    ]);
+
+    if (!fromUserData.game_user_id)
+      throw new Error(`User not found in ${from}`);
+    if (!toUserData.game_user_id) throw new Error(`User not found in ${to}`);
+
+    await Promise.all([
+      axios.post(`${fromGameData.endpoint}/burn`, {
+        amount: amountIn,
+      }),
+      axios.post(`${toGameData.endpoint}/mint`, {
+        amount: amountIn,
+      }),
+    ]);
+
+    await this.weaveDB.update(
+      {
+        amount0: from === address0 ? amount0 + amountIn : amount0 - amountOut,
+        amount1: from === address1 ? amount1 + amountIn : amount1 - amountOut,
+      },
+      "pairs",
+      `${address0}:${address1}`,
+      this.ethWallet
+    );
+  }
 }
